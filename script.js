cd732e1422e306303ece3190e0ca26f5086b853f
Kendall Carter <kendallacarter1@gmail.com>
2023-05-09 12:19:42 -0500

    fixed sliders

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-const volume = document.getElementById('volume')
-const bass = document.getElementById('bass')
-const treble = document.getElementById('treble')
-const middle = document.getElementById('mids')
-const visualizer = document.getElementById('visualizer')
+const volume = document.getElementById("volume")
+const bass = document.getElementById("bass")
+const treble = document.getElementById("treble")
+const middle = document.getElementById("mids")
+const visualizer = document.getElementById("visualizer")
 
-const mute = document.getElementById('mute')
-const play = document.getElementById('play')
-const pause = document.getElementById('pause')
+
 
 const context = new AudioContext()
 
 const analyzerNode = new AnalyserNode(context,{fftSize:512})
 
-const gainNode = new GainNode(context, {gain: volume.value})
-
+const gainNode = new GainNode(context, { gain: volume.value})
 const bassEq = new BiquadFilterNode(context, {
-    type:'lowshelf',
-    frequency: 500,
-    gain: bass.value
+  type: 'lowshelf',
+  frequency: 500,
+  gain: bass.value
 })
 const midEq = new BiquadFilterNode(context, {
-    type:'peaking',
-    Q: Math.SQRT1_2,
-    frequency: 1500,
-    gain: middle.value
+  type: 'peaking',
+  Q: Math.SQRT1_2,
+  frequency: 1500,
+  gain: mids.value
 })
 const trebleEq = new BiquadFilterNode(context, {
-    type:'highshelf',
-    frequency: 3000,
-    gain: treble.value
+  type: 'highshelf',
+  frequency: 3000,
+  gain: treble.value
 })
 
 setupEventListeners()
@@ -36,38 +33,44 @@ setupContext()
 resize()
 createVisualizer()
 
-function setupEventListeners(){
+
+function setupEventListeners() {
     window.addEventListener('resize', resize)
-    volume.addEventListener('input', e =>{
-        const value = parseFloat(e.target.value)
-        gainNode.gain.setTargetAtTime(value, context.currentTime, 0.01)
+  
+    volume.addEventListener('input', e => {
+      const value = parseFloat(e.target.value)
+      gainNode.gain.setTargetAtTime(value, context.currentTime, .01)
     })
-    bass.addEventListener('input', e =>{
-        const value = parseInt(e.target.value)
-        bassEq.gain.setTargetAtTime(value, context.currentTime, 0.01)
+  
+    bass.addEventListener('input', e => {
+      const value = parseInt(e.target.value)
+      bassEq.gain.setTargetAtTime(value, context.currentTime, .01)
     })
-    treble.addEventListener('input', e =>{
-        const value = parseInt(e.target.value)
-        trebleEq.gain.setTargetAtTime(value, context.currentTime, 0.01)
+  
+    mids.addEventListener('input', e => {
+      const value = parseInt(e.target.value)
+      midEq.gain.setTargetAtTime(value, context.currentTime, .01)
     })
-    middle.addEventListener('input', e =>{
-        const value = parseInt(e.target.value)
-        midEq.gain.setTargetAtTime(value, context.currentTime, 0.01)
+  
+    treble.addEventListener('input', e => {
+      const value = parseInt(e.target.value)
+      trebleEq.gain.setTargetAtTime(value, context.currentTime, .01)
     })
-}
+  }
 
 async function setupContext(){
     const instrument = await getInstrument()
-    if(context.state === 'suspended'){
+    if(context.state === "suspended"){
         await context.resume()
     }
     const source = context.createMediaStreamSource(instrument)
-    source.connect(gainNode)
-    source.connect(bassEq)
-    source.connect(midEq)
-    source.connect(trebleEq)
-    source.connect(analyzerNode)
-    source.connect(context.destination)
+    source
+      .connect(bassEq)
+      .connect(midEq)
+      .connect(trebleEq)
+      .connect(gainNode)
+      .connect(analyzerNode)
+      .connect(context.destination)
 }
 
 function getInstrument(){
@@ -91,13 +94,13 @@ function createVisualizer(){
     const height = visualizer.height
     const barWidth =    width / bufferLength
 
-    const canvasContext = visualizer.getContext('2d')
+    const canvasContext = visualizer.getContext("2d")
     canvasContext.clearRect(0,0,width,height)
     dataArray.forEach((item, index) => {
         const y = item / 255 * height /2
         const x = index * barWidth
-        canvasContext.fillStyle = 'rgb(0,0,0)'
-        canvasContext.fillRect(x*2, height - y, barWidth*2, y)
+        canvasContext.fillStyle = "rgb(0,0,0)"
+        canvasContext.fillRect(x*4, height - y, barWidth*4, y)
     })
 
 }
@@ -107,8 +110,8 @@ function resize(){
     visualizer.height = visualizer.clientHeight * window.devicePixelRatio
 }
 // function eventListeners(){
-//     window.addEventListener('resize', resize)
-//     volume.addEventListener('input', e =>{
+//     window.addEventListener("resize", resize)
+//     volume.addEventListener("input", e =>{
 //     const value = parseFloat(e.target.value)
 //     gainNode.gain.value = value
 // })
